d40d02e87f11f7a19e5822e1e275d35a2d8ae3f7
Mursula Otto <s2200126@BC-C02GR0BSQ6LR-MacBook-Air.local>
2022-02-14 09:38:20 +0200

    object exercise

--- objectsite/app.js
+++ objectsite/app.js
@@ -1,5 +1,6 @@
 let searchCars = document.querySelector("#searchCars");
 let submitButton = document.querySelector("#addcar");
+let salesPrices = document.querySelector("#salesPrices");
 
 class Car {
   constructor(licence_plate, maker, model, owner, price, color) {
@@ -9,22 +10,67 @@ class Car {
     this.owner = owner;
     this.price = price;
     this.color = color;
+    this.salePrice = function () {
+      if (this.price > 20000) return (this.price *= 0.75);
+      else if (this.price >= 15000) return (this.price *= 0.8);
+      else if (this.price < 15000) return (this.price *= 0.9);
+    };
   }
 }
 
 carsArray = [];
 
-// Tämä tuo objectin 
+// alenna hinnat
+
+salesPrices.addEventListener("click", () => {
+  let tableRow = `<tr>
+  <th>licence_place</th>
+  <th>maker</th>
+  <th>model</th>
+  <th>owner</th>
+  <th>price</th>
+  <th>color</th>
+</tr>`;
+  for (let car of carsArray) {
+    console.log(car);
+    car.salePrice();
+    tableRow +=
+      "<tr>" +
+      "<td>" +
+      car.licence_plate +
+      "</td>" +
+      "<td>" +
+      car.maker +
+      "</td>" +
+      "<td>" +
+      car.model +
+      "</td>" +
+      "<td>" +
+      car.owner +
+      "</td>" +
+      "<td>" +
+      car.price +
+      "</td>" +
+      "<td>" +
+      car.color +
+      "</td>" +
+      "</tr>";
+  }
+  document.querySelector(".tableData").innerHTML = tableRow;
+});
+
+// etsi auto
 
 searchCars.addEventListener("click", (e) => {
   console.log(carsArray);
   e.preventDefault();
   let input = document.querySelector("#searchInput").value;
   let results = document.querySelector("#searchResults");
-  tempArray = carsArray.filter((car) => car.licence_plate === input);
-  console.log(tempArray);
-  results.textContent = ` ${tempArray.licence_plate}`;
+  tempArray = carsArray.filter((car) => car.licence_plate == input);
+  results.textContent = `Car with a licence plate ${tempArray[0].licence_plate} is a ${tempArray[0].maker} is owned by ${tempArray[0].owner}`;
 });
+
+// Tämä tuo objectin taulukkoon
 
 submitButton.addEventListener("click", (e) => {
   e.preventDefault();
